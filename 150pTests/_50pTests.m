19b7c72e73e9c9614c6bd9a400ca8da6e4c21cf4
SeungyoupBaek <bsy8220@naver.com>
2014-01-06 14:46:19 +0900

    Initial Commit

---
+++ 150pTests/_50pTests.m
@@ -0,0 +1,34 @@
+//
+//  _50pTests.m
+//  150pTests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _50pTests : XCTestCase
+
+@end
+
+@implementation _50pTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
